3ba6fee3710e2c0b37e756898747dc8acf77e743
Binura99 <binurayasith12@gmail.com>
2024-11-01 17:20:07 +0530

    Add project files.

---
+++ Blood-Donation-Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blood_Donation_Backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blood_Donation_Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { 
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Blood-Donation-Backend/Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+
+namespace Blood_Donation_Backend.Models.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Mobile { get; set; }
+
+        public string? Email { get; set; }
+
+        public required string BloodGroup { get; set; }
+
+        public required string Address { get; set; }
+
+
+    }
+}
